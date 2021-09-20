acbd6c78188f5f7de9eff46cc3066931ef83d20d
Rywells88 <37821625+Rywells88@users.noreply.github.com>
2021-09-20 17:14:01 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var p = document.getElementById('text');
+p.innerHTML = '';
+var n = 0;
+var m = 0;
+var str = 'This is a test.';
+var typeTimer = setInterval(function() {
+  n = n + 1;
+  if(m === 0){
+  p.innerHTML = "> " + str.slice(0, n);
+    m = 1;
+  }else{
+    p.innerHTML = "> " + str.slice(0, n) + "|"
+    m = 0;
+  }
+  
+  if (n === str.length) {
+    clearInterval(typeTimer);
+    p.innerHTML = "> " + str;
+    n = 0;
+    setInterval(function() {
+
+      if (n === 0) {
+        p.innerHTML = "> " + str + "|"
+        n = 1;
+      } else {
+        p.innerHTML = "> " + str
+        n = 0;
+      };
+    }, 700);
+  };
+}, 500)
