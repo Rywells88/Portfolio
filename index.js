1398dc05c7abdddb2930fda0fe686b0ef8956b87
Rywells88 <37821625+Rywells88@users.noreply.github.com>
2021-11-14 12:19:32 -0500

    Build with javascript and html/css

--- index.js
+++ index.js
@@ -28,4 +28,30 @@ var typeTimer = setInterval(function() {
       };
     }, 700);
   };
-}, 500)
+}, 200);
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
